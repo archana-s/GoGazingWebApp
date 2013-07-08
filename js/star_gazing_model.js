7fffa2e41db12a14a29d18f6b271c97d78313d80
ArchanaS <archana.june@gmail.com>
2013-07-07 20:41:38 -0700

    A few additional files added.

---
+++ js/star_gazing_model.js
@@ -0,0 +1,11 @@
+GoGazeModel = Backbone.Model.extend ({
+    
+    city: 'Mountain View',
+    moonphase: 'New Moon',
+    latitude: 37.80,
+    longitude: 120.50,
+    
+    initialize: function() {
+        
+    }
+});
