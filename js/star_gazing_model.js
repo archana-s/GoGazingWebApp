7897e73ca457146c9cf49a375bf1fed2558d624c
ArchanaS <archana.june@gmail.com>
2013-07-20 17:57:04 -0700

    Reorganized the code to use a model that neatly holds all the weather and moon data.

--- js/star_gazing_model.js
+++ js/star_gazing_model.js
@@ -1,11 +1,124 @@
-GoGazeModel = Backbone.Model.extend ({
+var GoGazeModel = Backbone.Model.extend ({
     
     city: 'Mountain View',
     moonphase: 'New Moon',
-    latitude: 37.80,
-    longitude: 120.50,
-    
+   
     initialize: function() {
+        _.bindAll(this, "getDarkSpotsForTheRegion");
+        this.date = new Date();
+        this.latitude = geoplugin_latitude();
+        this.longitude = geoplugin_longitude();
+        this.spots = [];
+        this.darkNights = "";
+    },
+    
+    populateModel: function() {
         
-    }
+    },
+    
+    getMonthName: function (number) {
+        switch(number){
+                case 1: return 'Jan';
+                case 2: return 'Feb';
+                case 3: return 'Mar';
+                case 4: return 'Apr';
+                case 5: return 'May';
+                case 6: return 'Jun';
+                case 7: return 'Jul';
+                case 8: return 'Aug';
+                case 9: return 'Sep';
+                case 10: return 'Oct';
+                case 11: return 'Nov';
+                case 12: return 'Dec';
+                default: return 'error';
+        }
+    },
+    
+    getMoonPhaseForToday: function() {
+        var todayDate = new Date();
+        this.moonphase =  this.getMoonPhase (todayDate.getDate(), todayDate.getMonth()+1, todayDate.getFullYear());
+    },
+    
+    getMoonPhase: function (day, month, year) {    
+        /*
+	       modified from http://www.voidware.com/moon_phase.htm
+	   */
+	   var c,e,jd,b = 0;
+	   if (month < 3)
+	   {
+		  year = year - 1;
+		  month = month + 12;
+	   }
+
+	   month = month + 1;
+	   c = 365.25 * year;
+	   e = 30.6 * month;
+	   jd = c + e + day - 694039.09;	//jd is total days elapsed
+	   jd = jd/29.5305882;					//divide by the moon cycle
+	   b = Math.floor(jd);						//int(jd) -> b, take integer part of jd
+	   jd -= b;							//subtract integer part to leave fractional part of original jd
+	   b = Math.round(jd * 8);				//scale fraction from 0-8 and round
+
+	   if (b >= 8 )
+	   {  
+           b = 0;//0 and 8 are the same so turn 8 into 0
+	   }    
+       switch (b)
+	   {
+		  case 0:
+			 return 'New Moon';
+		  case 1:
+			 return 'Waxing Crescent Moon';
+		  case 2:
+			 return 'Quarter Moon';
+		  case 3:
+			 return 'Waxing Gibbous Moon';
+		  case 4:
+			 return 'Full Moon';
+		  case 5:
+			 return 'Waning Gibbous Moon';
+		  case 6:
+			 return 'Last Quarter Moon';
+		  case 7:
+			 return 'Waning Crescent Moon';
+		  default:
+			 return 'Error';
+	   }
+    },
+    
+    readDarkSpots: function() {
+        $.getJSON ('data/darkspots.json', this.getDarkSpotsForTheRegion);
+    },
+    
+    getDarkSpotsForTheRegion:function(data) {
+        this.spots = [];
+        var lat = this.latitude;
+        var long = this.longitude;
+        var self = this;
+        var index = 0;
+        $.each (data.spots, function(){
+            if (Math.abs(this.lat - lat) <= 3 && Math.abs(this.long - long) <= 3) {
+                var obj = {"name": this.name, "lat": this.lat, "long": this.long};
+                self.spots[index++] = obj;
+            }
+        });
+        this.trigger("fetchedDarkSpots");
+    },
+    
+     upcomingDarkNights: function(numDays) {
+        this.darkNights = "";
+        var index = 0;
+        for( var i=0;i<numDays;i++) {
+            var today = new Date();
+            var vardate = new Date();
+            vardate.setDate (today.getDate()+i);
+            var month = vardate.getMonth() + 1;
+            var moonphase = this.getMoonPhase (vardate.getDate(), month, vardate.getFullYear());
+            if (moonphase === 'New Moon') {
+               this.darkNights = this.darkNights + " " + vardate.getDate() + " " + this.getMonthName(month) + " " + vardate.getFullYear() + "   ";
+            }
+        }
+        return this.darkNights;
+    },
+    
 });
--- js/view_home_page.js
+++ js/view_home_page.js
@@ -5,21 +5,19 @@ HomePage = Backbone.View.extend({
     el: 'body',
     loader_img: '<img src="images/loader.gif"/>',
     current_chosen_city: 'Mountain View',
-    current_moonphase: 'New Moon',
     allowed_weather_failures: 3,
     num_weather_failures: 0,
-    current_latitude: 0,
-    current_longitude: 0,
     moon_phase_retrieved: false,
     weather_retrieved: false,
     city_info_retrieved: false,
     
     initialize: function() {
         _.bindAll (
-            this, "render", "extractWeatherInfo", "getDarkSpotsForTheRegion");
+            this, "render", "extractWeatherInfo", "updateDarkSpotsForTheRegion");
         this.render();
         this.cloudcover = "";
         this.visibility = "";
+        this.model.bind("fetchedDarkSpots", this.updateDarkSpotsForTheRegion);
     },
     
     events: {
@@ -33,34 +31,30 @@ HomePage = Backbone.View.extend({
         var that = this;
         this.initVars();
         
-        if(this.current_moonphase !== 'New Moon') {
+        if(this.model.moonphase !== 'New Moon') {
             $(".gaze_verdict").children().remove();
             $(".gaze_verdict").append("Bright Moon. Telescope recommended");
         }
         
-        var upcoming_dark_nights = this.upcomingDarkNights(45);
+        var upcoming_dark_nights = this.model.upcomingDarkNights(45);
         $.get('templates/darknights.html', function(templates) {  
-            // Fetch the <script /> block from the loaded external 
-            // template file which contains our greetings template.
             var template = $(templates).html();
             $('.moon_phase_section').children().remove();
-            $('.moon_phase_section').append(Mustache.render(templates, {"moon_phase": that.current_moonphase, "dark_nights_this_month":upcoming_dark_nights}));
+            $('.moon_phase_section').append(Mustache.render(templates, {"moon_phase": that.model.moonphase, "dark_nights_this_month":upcoming_dark_nights}));
         });
         
         $('.title_city').children().remove();
         $('.title_city').append(this.current_chosen_city);
         // Get weather conditions here 
         this.getWeatherConditionsWithLatLong(geoplugin_latitude(), geoplugin_longitude());
-        this.readDarkSpots();
         this.putLoader();
         return this;
     },
     
     initVars: function () {
         this.current_chosen_city = geoplugin_city();
-        this.current_moonphase = this.getMoonPhaseForToday();
-        this.current_latitude = geoplugin_latitude();
-        this.current_longitude = geoplugin_longitude();
+        this.model.getMoonPhaseForToday();
+        this.model.readDarkSpots();
     },
     
     haveAllInfo: function() {
@@ -73,7 +67,7 @@ HomePage = Backbone.View.extend({
     updateGazeCondition: function(cloud_cover) {
         var gaze_verdict = "Bright moon. Telescope recommended";
         cloud_cover = parseInt(cloud_cover, 10);
-        if (this.current_moonphase === "New Moon") {
+        if (this.model.moonphase === "New Moon") {
             if (cloud_cover <= 15) {
                 gaze_verdict = "Great dark night to gaze.";
             }
@@ -102,94 +96,7 @@ HomePage = Backbone.View.extend({
         $(".dark_spots").append(this.loader_img);
     },
     
-    getMonthName: function (number) {
-        switch(number){
-                case 1: return 'Jan';
-                case 2: return 'Feb';
-                case 3: return 'Mar';
-                case 4: return 'Apr';
-                case 5: return 'May';
-                case 6: return 'Jun';
-                case 7: return 'Jul';
-                case 8: return 'Aug';
-                case 9: return 'Sep';
-                case 10: return 'Oct';
-                case 11: return 'Nov';
-                case 12: return 'Dec';
-                default: return 'error';
-        }
-    },
-    
-    getMoonPhaseForToday: function() {
-        var todayDate = new Date();
-        return (this.getMoonPhase (todayDate.getDate(), todayDate.getMonth()+1, todayDate.getFullYear()));
-    },
-    
-    upcomingDarkNights: function(numDays) {
-        var results = "";
-        var index = 0;
-        for( var i=0;i<numDays;i++) {
-            var today = new Date();
-            var vardate = new Date();
-            vardate.setDate (today.getDate()+i);
-            var month = vardate.getMonth() + 1;
-            var moonphase = this.getMoonPhase (vardate.getDate(), month, vardate.getFullYear());
-            if (moonphase === 'New Moon') {
-               results = results + " " + vardate.getDate() + " " + this.getMonthName(month) + " " + vardate.getFullYear() + "   ";
-            }
-        }
-        return results;
-    },
-    
-    getMoonPhase: function (day, month, year) {    
-        /*
-	       modified from http://www.voidware.com/moon_phase.htm
-	   */
-	   var c,e,jd,b = 0;
-	   if (month < 3)
-	   {
-		  year = year - 1;
-		  month = month + 12;
-	   }
-
-	   month = month + 1;
-	   c = 365.25 * year;
-	   e = 30.6 * month;
-	   jd = c + e + day - 694039.09;	//jd is total days elapsed
-	   jd = jd/29.5305882;					//divide by the moon cycle
-	   b = Math.floor(jd);						//int(jd) -> b, take integer part of jd
-	   jd -= b;							//subtract integer part to leave fractional part of original jd
-	   b = Math.round(jd * 8);				//scale fraction from 0-8 and round
-
-	   if (b >= 8 )
-	   {  
-           b = 0;//0 and 8 are the same so turn 8 into 0
-	   }    
-       this.updateRetrievalInfo ("moon_phase"); 
-	   switch (b)
-	   {
-		  case 0:
-			 return 'New Moon';
-		  case 1:
-			 return 'Waxing Crescent Moon';
-		  case 2:
-			 return 'Quarter Moon';
-		  case 3:
-			 return 'Waxing Gibbous Moon';
-		  case 4:
-			 return 'Full Moon';
-		  case 5:
-			 return 'Waning Gibbous Moon';
-		  case 6:
-			 return 'Last Quarter Moon';
-		  case 7:
-			 return 'Waning Crescent Moon';
-		  default:
-			 return 'Error';
-	   }
-    },
-    
-    updateRetrievalInfo: function (retrieved_value) {
+   /* updateRetrievalInfo: function (retrieved_value) {
         switch(retrieved_value) {
                 case "moon_phase":
                     this.moon_phase_retrived = true;
@@ -211,7 +118,7 @@ HomePage = Backbone.View.extend({
     },
         
     addPageLoader: function() {
-    },
+    },*/
     
     getWeatherConditionsWithLatLong: function(lat, long) {
         var geo_specs = lat + "+" + long;
@@ -229,7 +136,6 @@ HomePage = Backbone.View.extend({
     },
     
     weatherFetchFailed: function (data) {
-        debugger;
         this.num_weather_failures ++;
         if (this.num_weather_failures > this.allowed_weather_failures) {
             this.num_weather_failures = 0;
@@ -240,7 +146,7 @@ HomePage = Backbone.View.extend({
             if(this.num_weather_failures === 1) {
                 $('.location_weather_section').append("Oops! I am having troubles fetching weather. Trying again!");
             }
-            this.getWeatherConditionsWithLatLong(this.current_latitude, this.current_longitude);
+            this.getWeatherConditionsWithLatLong(this.model.latitude, this.model.longitude);
         }
     },
     
@@ -254,7 +160,7 @@ HomePage = Backbone.View.extend({
         $.get('templates/location_info.html', function(templates) {  
             var template = $(templates).html();
             $('.location_weather_section').children().remove();
-            $('.location_weather_section').append(Mustache.render(templates, {"cloud_cover": cloud_cover, "visibility": visibility, "moon_phase": self.current_moonphase}));
+            $('.location_weather_section').append(Mustache.render(templates, {"cloud_cover": cloud_cover, "visibility": visibility, "moon_phase": self.model.moonphase}));
         });
     },
     
@@ -300,46 +206,28 @@ HomePage = Backbone.View.extend({
         
         var url = "http://maps.googleapis.com/maps/api/geocode/json?address="+ inputLocation +"&sensor=false";
         $.getJSON(url).success (function(data){
-            self.current_latitude = data.results[0].geometry.location.lat;
-            self.current_longitude = data.results[0].geometry.location.lng;
-            self.getWeatherConditionsWithLatLong(self.current_latitude, self.current_longitude);
+            self.model.latitude = data.results[0].geometry.location.lat;
+            self.model.longitude = data.results[0].geometry.location.lng;
+            self.getWeatherConditionsWithLatLong(self.model.latitude, self.model.longitude);
             /** 3. Update Dark spots 
                 */
-            self.readDarkSpots (self.current_latitude, self.current_longitude);
+            self.model.readDarkSpots();
         });
     },
     
     clearOutGazingCondition: function() {
-        if(this.current_moonphase === 'New Moon') {
+        if(this.model.moonphase === 'New Moon') {
             $('.gaze_verdict').children().remove();
             $('.gaze_verdict').html(this.loader_img);
         }
     },
-   
-    readDarkSpots: function(lat, long) {
-        $.getJSON ('data/darkspots.json', this.getDarkSpotsForTheRegion);
-    },
     
-    getDarkSpotsForTheRegion:function(data) {
-        var lat = this.current_latitude;
-        var long = this.current_longitude;
-        
-        var spots = [];
-        var index = 0;
-        $.each (data.spots, function(){
-            if (Math.abs(this.lat - lat) <= 3 && Math.abs(this.long - long) <= 3) {
-                var obj = {"name": this.name, "lat": this.lat, "long": this.long};
-                spots[index++] = obj;
-            }
-        });
-       
-        //render content in HTML
+    updateDarkSpotsForTheRegion:function() {
+        var self = this;
         $.get('templates/gaze_spots.html', function(templates) {  
-            // Fetch the <script /> block from the loaded external 
-            // template file which contains our greetings template.
             var template = $(templates).html();
             $('.dark_spots').children().remove();
-            $('.dark_spots').append(Mustache.render(templates, {"spots": spots}));
+            $('.dark_spots').append(Mustache.render(templates, {"spots": self.model.spots}));
         });
     },
     
@@ -382,7 +270,8 @@ HomePage = Backbone.View.extend({
 });
 
 $(document).ready (function(){
-    homepage = new HomePage();
+    var gogazingModel = new GoGazeModel();
+    homepage = new HomePage({model: gogazingModel});
     //console.log (geoplugin_latitude() + " " + geoplugin_longitude());
 });
 
